f0a73c23bad5e073282c0689763777b0b5ed28c6
Ole <ole@pixelogik.de>
2015-06-05 20:27:57 +0200

    Increased version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='NearPy',
-    version='0.2.1',
+    version='0.2.2',
     author='Ole Krause-Sparmann',
     author_email='ole@pixelogik.de',
     packages=[
